f3c567f310897ccbdd6754239af3d6d0e429b642
graph10 <142536843+graph10@users.noreply.github.com>
2024-06-15 22:04:29 +0300

    Update middleware.py

--- Promotion/PromotionGuide/main/middleware.py
+++ Promotion/PromotionGuide/main/middleware.py
@@ -2,17 +2,25 @@ from datetime import timedelta
 from django.utils.deprecation import MiddlewareMixin
 from django.utils import timezone
 from django.http import HttpResponseRedirect
+import datetime
 
 class SessionTimeoutMiddleware(MiddlewareMixin):
     def process_request(self, request):
         # Проверяем, существует ли атрибут 'session' в объекте 'request'
         if hasattr(request, 'session'):
             if 'last_activity' in request.session:
-                session_expires = request.session.get_decoded().get('expiry')
+                session_expires = datetime.utcfromtimestamp(session_expires)
                 current_time = timezone.now()
-                time_difference = session_expires - current_time
-                if time_difference <= timedelta(days=7):
+                time_difference = datetime.fromtimestamp(session_expires) - current_time
+                if time_difference > timedelta(days=7):
                     # Удалить сессию, если она истекла
                     request.session.flush()
                     # Используйте HttpResponseRedirect для перенаправления
                     return HttpResponseRedirect('/')
+                
+    def process_response(self, request, response):
+        if hasattr(request, 'session') and 'last_activity' in request.session:
+            request.session['last_activity'] = timezone.now()
+            expiry = timezone.now() + timedelta(days=7)
+            request.session['expiry'] = expiry.total_seconds()
+        return response
